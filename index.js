2b0c619d6688e42e43ea7cf4a4cd6ae9dffe7b12
2M0N <tumon@Debian11.2M0N>
2022-04-03 05:43:12 +0600

    init

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ app.use(cookie())
 app.use(urlencoded({limit: '50mb',extended:false}))
 app.use(express.json({limit: '50mb'}))
 app.use(cors(
-//     {
-//     credentials:true,
-//     origin: true,
+    {
+    // credentials:true,
+    oirgin: true,
 
-//     // origin: ["http://news-stand-next.vercel.app", /\.news-stand-next.vercel\.app$/]
-// }
+    // origin: ["http://news-stand-next.vercel.app", /\.news-stand-next.vercel\.app$/]
+}
 ))
 
 app.use(device.capture());
